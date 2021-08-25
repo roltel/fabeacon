a17118b2f35f6540e59da21fbadcbd6e2397e6e1
Rolando Reset <roltel@roltel.net>
2021-08-25 12:11:27 +0100

    Update beacon.sh

--- beacon.sh
+++ beacon.sh
@@ -3,9 +3,9 @@ wget -O /home/pi/fabeacon.mp3 https://futuragora.pt/futurai/radio/boletim/fabeac
 #wget -O /home/pi/mmbeacon.wav https://futuragora.pt/futurai/radio/boletim/mmbeacon.wav
 python /home/pi/fabeacon/on.py
 #sleep 1
-mplayer /home/pi/333-jing1.mp3
+mplayer /home/pi/fabeacon/audio/mac-jingle.mp3
 mplayer /home/pi/fabeacon.mp3
-mplayer /home/pi/333-jing1.mp3
+mplayer /home/pi/fabeacon/audio/mac-jingle.mp3
 sleep 1
 python /home/pi/fabeacon/off.py
 
